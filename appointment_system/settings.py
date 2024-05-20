2112718c27ffaa4ef4efe338422370f720b7ab99
Tech Support <techsupport@edoofa.com>
2024-05-20 13:12:45 +0530

    Create static directory and update settings

--- appointment_system/settings.py
+++ appointment_system/settings.py
@@ -139,9 +139,7 @@ MIDDLEWARE = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Simplified static file serving.
 # https://whitenoise.readthedocs.io/en/stable/django.html#django-middleware
